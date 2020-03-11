9c5e85b60aef942bc9546b72154141bedc7cc805
Junsung Park <zeipar@gmail.com>
2020-03-11 20:37:35 +0000

    refactored that connection stays in client.js and everything related to play remains in play.js

--- play.js
+++ play.js
@@ -1,37 +1,4 @@
-const net = require('net');
-const stdin = process.stdin;
-
-stdin.setEncoding('utf8');
-stdin.resume();
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-  
-  conn.on('data', (data) => {
-    console.log(data);
-  });
-  return conn;
-}
-
-// stdin.on('data', data => {
-//   if(data === '\\q\n') {
-//     // close client
-//     // client.end();
-//     process.exit();
-//   }
-//   console.log(data);
-//   //conn.write(data);
-// })
-
-
+const connect = require('./client');
 
 console.log('Connecting ...');
 connect();
