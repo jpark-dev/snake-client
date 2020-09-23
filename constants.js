5eab5ea519762971c0d7d7bca4816e25cd3d93e3
Jasom Park <zeipar@gmail.com>
2020-09-23 15:24:25 -0700

    linted.

--- constants.js
+++ constants.js
@@ -1,7 +1,7 @@
-const IP = '192.168.88.177';
+const IP = "localhost";
 const PORT = 50541;
 
 module.exports = {
   IP,
-  PORT
+  PORT,
 };
--- input.js
+++ input.js
@@ -1,34 +1,33 @@
 let connection;
 
-const handleUserInput = (key) => {
-  if (key === '\u0003') {
+const handleUserInput = key => {
+  if (key === "\u0003") {
     process.exit();
   }
-  if (key === 'a') {
-    connection.write('Move: left');
+  if (key === "a") {
+    connection.write("Move: left");
   }
-  if (key === 'w') {
-    connection.write('Move: up');
+  if (key === "w") {
+    connection.write("Move: up");
   }
-  if (key === 'd') {
-    connection.write('Move: right');
+  if (key === "d") {
+    connection.write("Move: right");
   }
-  if (key === 's') {
-    connection.write('Move: down');
+  if (key === "s") {
+    connection.write("Move: down");
   }
-  if (key === 'c') {
-    connection.write('Say: MOV');
+  if (key === "c") {
+    connection.write("Say: MOV");
   }
-
 };
 
-const setupInput = function(conn) {
+const setupInput = conn => {
   const stdin = process.stdin;
   connection = conn;
   stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
+  stdin.setEncoding("utf8");
   stdin.resume();
-  stdin.on('data', handleUserInput);
+  stdin.on("data", handleUserInput);
   return stdin;
 };
 
